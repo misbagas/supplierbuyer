116606b1f89231c2c7bd93b3b9c87533c1cfbc28
misbagas <misbahbagaskara@gmail.com>
2026-02-14 19:02:44 +0700

    update server path and folder name

--- supplierbuyer.cpp
+++ supplierbuyer.cpp
@@ -697,7 +697,7 @@ public:
 int main() {
 
     const char* options[] = {
-        "document_root", "./",
+        "document_root", ".",
         "listening_ports", "8080",
         "error_log_file", "/root/supplierbuyer/error.log",
         "enable_directory_listing", "no",
